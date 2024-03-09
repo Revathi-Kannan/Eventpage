5b47786ed18f1a01c5169978fb5f6da928e1c6b7
Revathi kannan <154062101+Revathi-Kannan@users.noreply.github.com>
2024-03-10 00:19:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector(".Clear").addEventListener("click", function(){
+document.querySelectorAll("input").forEach(function(input){
+    input.value=""
+})
+
+
+})
+
+document.querySelector(".Submit").addEventListener("click", function(){
+document.querySelectorAll("input").forEach(function(input){
+    input.value=""
+})
+
+})
